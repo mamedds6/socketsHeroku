089e415fc4791714eed8593ef60dfbb331085b05
mamedds6 <darekmichalski@onet.eu>
2020-08-03 16:18:56 +0300

    fix 25 ms

--- server.js
+++ server.js
@@ -19,20 +19,20 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
-// setInterval(() => {
-//   io.emit('time', new Date().toTimeString());
-//   console.log("tick")
-// },1000);
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
 
 var i = 0;
 
 setInterval(() => {  
   i += 1;
   io.emit('fast', i)
-  //console.log("tick")
-},5);
+  //console.log("tik " + i)
+},25);
+
+setInterval(() => {
+  io.emit('time', new Date().toTimeString(), i);
+  //console.log("------- " + i)
+},1000);
